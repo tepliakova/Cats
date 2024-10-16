47bc126042864c1c18fdcc7634d46779da77ae0a
Proba <teplyakovateplyakova94@gmail.com>
2024-10-16 22:43:36 +1000

    Добавили изменение размера изображения

--- Catsaas.py
+++ Catsaas.py
@@ -10,6 +10,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
@@ -25,7 +26,7 @@ def set_image():
 
 window = Tk()
 window.title("Cats")
-window.geometry("600x480")
+window.geometry("600x520")
 
 label = Label()
 label.pack()
