d1dce1fc07a819f0d26b60c8ea787225fc1804df
Proba <teplyakovateplyakova94@gmail.com>
2024-10-16 22:57:28 +1000

    Теперь каждое изображение открывается в новом окне

--- Catsaas.py
+++ Catsaas.py
@@ -17,11 +17,15 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
 
 
@@ -32,9 +36,6 @@ window = Tk()
 window.title("Cats")
 window.geometry("600x520")
 
-label = Label()
-label.pack()
-
 # update_button = Button(text="Обновить", command=set_image)
 # update_button.pack()
 
@@ -44,7 +45,7 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command = set_image)
+file_menu.add_command(label="Загрузить фото", command = open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
