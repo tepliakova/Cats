ec192dc048c0a3bd9833346610d9c90ed715c155
Proba <teplyakovateplyakova94@gmail.com>
2024-10-16 22:33:54 +1000

    Добавили кнопку для загрузки нового изображения.

--- Catsaas.py
+++ Catsaas.py
@@ -4,7 +4,7 @@ import requests
 from io import BytesIO
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -15,6 +15,14 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set-image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
 
 window = Tk()
 window.title("Cats")
@@ -23,11 +31,10 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "https://cataas.com/cat"
+
 
 window.mainloop()
